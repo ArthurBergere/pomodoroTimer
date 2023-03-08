b8e5bcaf49efc72cc8e60a8760f52a039d9787c3
Arthur BERGERE <arthur.bergere@hutchinson.com>
2023-03-08 16:22:41 +0100

    [FIX] fix le timer + récompense chaque 25 minutes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+// Importer l'objet Audio
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  implements OnInit {
-
+  // Importer l'objet Audio
+  startSound = new Audio('../assets/bell.wav');
   headingText: string | undefined;
   remainingTime = 25 * 60 * 1000; // 25 minutes in milliseconds
   timerInterval: any;
+  numOfWorkSessionsCompleted = 0;
   displayTime: string | undefined;
   isTimerStarted = false;
   randomPhrases: string[] = [
@@ -26,11 +28,18 @@ export class AppComponent  implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Récupérer la valeur stockée précédemment
+    const savedNumOfWorkSessionsCompleted = localStorage.getItem('numOfWorkSessionsCompleted');
+    // Si la valeur existe, l'initialiser à `numOfWorkSessionsCompleted`
+    if (savedNumOfWorkSessionsCompleted) {
+      this.numOfWorkSessionsCompleted = parseInt(savedNumOfWorkSessionsCompleted);
+    }
     this.displayTime = this.getDisplayTime(this.remainingTime);
     this.headingText = "Pomodoro for study"
   }
 
   startTimer() {
+    this.startSound.play();
     this.isTimerStarted = true;
     this.headingText = "Let's start !"
     this.timerInterval = setInterval(() => {
@@ -38,11 +47,22 @@ export class AppComponent  implements OnInit {
       this.displayTime = this.getDisplayTime(this.remainingTime);
       if (this.remainingTime === 0) {
         clearInterval(this.timerInterval);
-        this.remainingTime = 5 * 60 * 1000; // 5 minutes in milliseconds
-        this.displayTime = this.getDisplayTime(this.remainingTime);
-        setTimeout(() => {
-          this.startTimer();
-        }, 5000);
+          // If the timer has not been paused, start the 5 minute break
+          this.numOfWorkSessionsCompleted += 1;
+          localStorage.setItem('numOfWorkSessionsCompleted', this.numOfWorkSessionsCompleted.toString());
+          this.remainingTime = 5 * 60 * 1000; // 5 minutes in milliseconds
+          this.headingText = "Take a break!";
+          this.timerInterval = setInterval(() => {
+            this.remainingTime -= 1000;
+            this.displayTime = this.getDisplayTime(this.remainingTime);
+            if (this.remainingTime === 0) {
+              clearInterval(this.timerInterval);
+              this.remainingTime = 25 * 60 * 1000; // 25 minutes in milliseconds
+              this.displayTime = this.getDisplayTime(this.remainingTime);
+              this.startTimer();
+            }
+          }, 1000);
+
       }
     }, 1000);
   }
@@ -63,6 +83,7 @@ export class AppComponent  implements OnInit {
     const seconds = ((time % 60000) / 1000).toFixed(0);
     return `${minutes}:${Number(seconds) < 10 ? '0' : ''}${seconds}`;
   }
+
   setRandomHeadingText() {
     setInterval(() => {
       const element = document.getElementById('heading');
@@ -73,7 +94,7 @@ export class AppComponent  implements OnInit {
         }, 1000);
       }
       this.headingText = this.randomPhrases[Math.floor(Math.random() * this.randomPhrases.length)];
-    }, 20000);
+    }, 30000);
   }
 }
   
